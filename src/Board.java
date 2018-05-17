5ca037d8cb1b93350cd416de70ea9205fe9cd463
bethechr000 <bethechr000@warren.k12.in.us>
2018-05-17 11:08:32 -0400

    Ludo or Sorry! ??

--- src/Board.java
+++ src/Board.java
@@ -14,20 +14,6 @@ public class Board extends JPanel implements ActionListener {
 
     }
 
-
-
-    public setup(){
-
-
-
-    }
-
-    public checkCollisions(){
-
-
-
-    }
-
     public void paintComponent(Graphics g){
         super.paintComponent(g);
     }
